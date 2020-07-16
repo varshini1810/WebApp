1a5c35108c256ef0d498a25b3a0bf39866bb0cd2
varshini1810 <60035333+varshini1810@users.noreply.github.com>
2020-07-16 12:50:33 +0530

    Update createpage.java

--- src/test/java/servlet/createpage.java
+++ src/test/java/servlet/createpage.java
@@ -17,7 +17,7 @@ public class createpage extends HttpServlet {
 		try
 		{
 			Class.forName("org.postgresql.Driver");
-			String jdbcUrl="jdbc:postgresql://52.15.185.180:5432/postgres";
+			String jdbcUrl="jdbc:postgresql://18.220.197.108:5432/postgres";
 			String username="postgres";
 			String password="";
 		
